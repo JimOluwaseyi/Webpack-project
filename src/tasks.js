967a8621416d5491dc1e15c5c7a9d50c9c535b38
jimoh oluwaseyi <heysey128@gmail.com>
2023-10-11 19:41:36 +0100

    Add the functionality

--- src/tasks.js
+++ src/tasks.js
@@ -1,57 +1,78 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("task")) || [];
+
+const setupDeleteTask = (taskEle, index) => {
+  const deleteIcon = taskEle.querySelectorAll(".right .fa-ellipsis-vertical");
+  if (deleteIcon) {
+    deleteIcon.forEach((del) => {
+      del.addEventListener("click", () => {
+        taskRemoval(index);
+      });
+    });
+  }
+};
+
+
+const editTask = (taskElement, icon, edit) => {
+  taskElement.addEventListener("dblclick", () => {
+    if (icon.classList.contains("fa-ellipsis-vertical")) {
+      icon.classList.remove("fa-ellipsis-vertical");
+      icon.classList.add("fa-trash-can");
+      taskElement.style.backgroundColor = "#ffff8752";
+      edit.contentEditable = true;
+      edit.focus();
+    } else {
+      icon.classList.remove("fa-trash-can");
+      icon.classList.add("fa-ellipsis-vertical");
+      taskElement.style.backgroundColor = "";
+    }
+  });
+
+  edit.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      edit.contentEditable = false;
+      icon.classList.remove("fa-trash-can");
+      icon.classList.add("fa-ellipsis-vertical");
+      taskElement.style.backgroundColor = "";
+    }
+  });
+};
 
 const generateTaskElement = (task) => {
-  const taskElement = document.createElement('li');
-  taskElement.classList.add('task');
+  const taskElement = document.createElement("li");
+  taskElement.classList.add("task");
   taskElement.innerHTML = `
       <div class="left">
-        <input type="checkbox"/>
-        <span>${task.description}</span>
+        <input type="checkbox" class="checkbox"/>
+        <div id="edit">${task.description}</div>
       </div>
       <div class="right">
         <i class="fa-solid fa-ellipsis-vertical"></i>
       </div>
   `;
-  const deleteIcon = taskElement.querySelectorAll(
-    '.right .fa-ellipsis-vertical',
-  );
+  const rightIcon = taskElement.querySelector(".fa-solid");
+  const edit = taskElement.querySelector("#edit");
+  setupDeleteTask(taskElement, task.index);
+  editTask(taskElement, rightIcon, edit);
 
-  const taskRemoval = (deleteTask) => {
-    tasks = tasks.filter((task) => task.index !== deleteTask);
-    populateList();
-  };
-
-  if (deleteIcon) {
-    deleteIcon.forEach((del) => {
-      del.addEventListener('click', () => {
-        taskRemoval(task.index);
-      });
-    });
-  }
-
-  taskElement.addEventListener('dblclick', (e) => {
-    const rightIcon = e.currentTarget.querySelector('.fa-solid');
-    if (rightIcon.classList.contains('fa-ellipsis-vertical')) {
-      rightIcon.classList.remove('fa-ellipsis-vertical');
-      rightIcon.classList.add('fa-trash-can');
-      taskElement.style.backgroundColor = '#ffff8752';
-    } else {
-      rightIcon.classList.remove('fa-trash-can');
-      rightIcon.classList.add('fa-ellipsis-vertical');
-      taskElement.style.backgroundColor = '';
-    }
-  });
   return taskElement;
 };
 
 const populateList = () => {
-  const taskList = document.querySelector('#taskList');
-  taskList.innerHTML = '';
-
+  const taskList = document.querySelector("#taskList");
+  taskList.innerHTML = "";
   tasks.forEach((task) => {
     const taskElement = generateTaskElement(task);
     taskList.appendChild(taskElement);
   });
+};
+
+const taskRemoval = (deleteTask) => {
+  tasks = tasks.filter((task) => task.index !== deleteTask);
+  tasks.forEach((task, idx) => {
+    task.index = idx + 1;
+    console.log(task);
+  });
+  populateList();
 };
 
 const addNewTask = (title) => {
@@ -60,7 +81,8 @@ const addNewTask = (title) => {
     completed: false,
     index: tasks.length + 1,
   });
-  // console.log(tasks);
+  console.log(tasks);
+  localStorage.setItem("task", JSON.stringify(tasks))
 };
 
 export { addNewTask, populateList };
